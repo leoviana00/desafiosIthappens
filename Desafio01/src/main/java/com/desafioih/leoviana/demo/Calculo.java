5878ff56306f3ea161c6bb01e6efa39f8e79b4c8
leoviana00 <leonardoviana00@hotmail.com>
2020-05-07 17:34:43 -0300

    ALteração da classe calculo, criação de uma classe de teste com junit e adicição de pastas para os outros desafios

---
+++ Desafio01/src/main/java/com/desafioih/leoviana/demo/Calculo.java
@@ -0,0 +1,9 @@
+package com.desafioih.leoviana.demo;
+
+public class Calculo {
+    public static float ExecutaCalculo (float Valor1,float Valor2)
+    {
+        float  Soma = Valor1 + Valor2 ;
+        return Soma;
+    }
+}
--- Desafio01/src/main/java/com/desafioih/leoviana/demo/soma.java
+++
@@ -1,7 +0,0 @@
-package com.desafioih.leoviana.demo;
-public class soma {
-    public static void main(String args[]) { 
-        int calculus = 1 + 1;
-        System.out.println(calculus);
-    } 
-}
